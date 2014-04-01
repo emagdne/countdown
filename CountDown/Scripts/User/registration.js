e89e52d7b3f454aee71b959ecbbb5a7001d542fd
Jordan Brown <emagdne@gmail.com>
2014-04-01 00:18:33 -0400

    Wrote boilerplate code for registration and error pages. Added nav features to layout. Applied various CSS fixes.

---
+++ CountDown/Scripts/User/registration.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    assignListeners();
+    $("#reg-fn").focus();
+
+    function assignListeners() {
+        $("button:contains('Clear')").click(function(e) {
+            $(".col-sm-9 > input").val("");
+            return false;
+        });
+    };
+});
