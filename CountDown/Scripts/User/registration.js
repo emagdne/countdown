991cd2ba70e4cc39c7506bd9d12bf30c9bf34c98
Jordan Brown <emagdne@gmail.com>
2014-04-02 01:18:25 -0400

    Added validation to the Registration view with some javascript and css enhancements. Renamed Index title.

--- CountDown/Scripts/User/registration.js
+++ CountDown/Scripts/User/registration.js
@@ -1,6 +1,7 @@
 ﻿$(function () {
     assignListeners();
-    $("#reg-fn").focus();
+    correctLabelPositions();
+    focusInputElement();
 
     function assignListeners() {
         $("button:contains('Clear')").click(function(e) {
@@ -8,4 +9,26 @@
             return false;
         });
     };
+
+    // Fixes issue with the label positions being offset by validation errors
+    // printed on top of input elements.
+    function correctLabelPositions() {
+        $(".form-group").each(function (index, element) {
+            element = $(element);
+            if (element.find("span.field-validation-error").size()) {
+                element.find("label").css({
+                    paddingTop: "25px"
+                });
+            }
+        });
+    }
+
+    function focusInputElement() {
+        var errors = $("span.field-validation-error");
+        if (!errors.size()) {
+            $("#reg-fn").focus();
+        } else {
+            errors.eq(0).next().focus();
+        }
+    }
 });
