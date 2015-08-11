9602e0012a6d8811a5d280fe74251a79cf674f93
Jordan Brown <emagdne@gmail.com>
2015-08-11 17:31:58 -0400

    Fix EF exceptions thrown by CountDownMembershipProvider class
    See CountDownMembershipProvider comment:
    
    The methods below access UserContext directly for a good reason. I tried instantiating UserRepository
    here, but Entity Framework threw some strange exceptions due to the fact that UserRepository kept
    a DbContext object in memory over the span of multiple requests.
    
    For future reference, the main exception thrown was EntityCommandExecutionException, followed by a variety
    of inner exceptions such as SQLite's "library routine called out of sequence\r\nnull connection or database handle".

--- CountDown/Models/Repository/UserRepository.cs
+++ CountDown/Models/Repository/UserRepository.cs
@@ -12,8 +12,6 @@ namespace CountDown.Models.Repository
     public interface IUserRepository
     {
         void InsertUser(User user);
-        bool AuthenticateUser(string email, string password);
-        User FindUserByEmail(string email);
         IEnumerable<User> AllUsers();
         void SaveChanges();
     }
@@ -24,8 +22,8 @@ namespace CountDown.Models.Repository
     /// </summary>
     public class UserRepository : IUserRepository
     {
-        private readonly PasswordHasher _passwordHasher;
         private readonly UserContext _db;
+        private readonly PasswordHasher _passwordHasher;
 
         public UserRepository()
         {
@@ -38,20 +36,6 @@ namespace CountDown.Models.Repository
             // Hash the password before persisting to DB
             user.Hash = _passwordHasher.HashPassword(user.Password);
             _db.Users.Add(user);
-        }
-
-        public bool AuthenticateUser(string email, string password)
-        {
-            var user = _db.Users.FirstOrDefault(x => x.Email.Equals(email));
-            if (user == null) return false;
-            var verification = _passwordHasher.VerifyHashedPassword(user.Hash, password);
-            return verification == PasswordVerificationResult.Success
-                   || verification == PasswordVerificationResult.SuccessRehashNeeded;
-        }
-
-        public User FindUserByEmail(string email)
-        {
-            return _db.Users.FirstOrDefault(x => x.Email.Equals(email));
         }
 
         public IEnumerable<User> AllUsers()
--- CountDown/Models/Security/CountDownMembershipProvider.cs
+++ CountDown/Models/Security/CountDownMembershipProvider.cs
@@ -1,5 +1,7 @@
-﻿using System.Web.Security;
+﻿using System.Linq;
+using System.Web.Security;
 using CountDown.Models.Repository;
+using Microsoft.AspNet.Identity;
 
 namespace CountDown.Models.Security
 {
@@ -16,44 +18,60 @@ namespace CountDown.Models.Security
     /// </summary>
     public class CountDownMembershipProvider : MembershipProvider
     {
-        private readonly IUserRepository _userRepository;
-
         public CountDownMembershipProvider()
         {
-            _userRepository = new UserRepository();
-        }
-
-        public CountDownMembershipProvider(IUserRepository userRepository)
-        {
-            _userRepository = userRepository;
         }
 
         #region Overrides
 
+        /* 
+         * The methods below access UserContext directly for a good reason. I tried instantiating UserRepository
+         * here, but Entity Framework threw some strange exceptions due to the fact that UserRepository kept
+         * a DbContext object in memory over the span of multiple requests.
+         *
+         * For future reference, the main exception thrown was EntityCommandExecutionException, followed by a variety
+         * of inner exceptions such as SQLite's "library routine called out of sequence\r\nnull connection or database handle".
+         */
+
         public override MembershipUser GetUser(string email, bool userIsOnline)
         {
-            var user = _userRepository.FindUserByEmail(email);
-            if (user == null) return null;
-
-            return new CountDownMembershipUser(user);
+            using (var context = new UserContext())
+            {
+                var user = context.Users.FirstOrDefault(x => x.Email.Equals(email));
+                return user == null ? null : new CountDownMembershipUser(user);
+            }
         }
 
         public override bool ValidateUser(string email, string password)
         {
-            return !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(password) && _userRepository.AuthenticateUser(email, password);
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password)) return false;
+            using (var context = new UserContext())
+            {
+                var user = context.Users.FirstOrDefault(x => x.Email.Equals(email));
+                var verification = PasswordVerificationResult.Failed;
+                if (user != null)
+                {
+                    var passwordHasher = new PasswordHasher();
+                    verification = passwordHasher.VerifyHashedPassword(user.Hash, password);
+                }
+                return user != null && (verification == PasswordVerificationResult.Success
+                                        || verification == PasswordVerificationResult.SuccessRehashNeeded);
+            }
         }
 
         #endregion
 
         #region Not Implemented
 
-        public override MembershipUser CreateUser(string username, string password, string email, string passwordQuestion, string passwordAnswer,
+        public override MembershipUser CreateUser(string username, string password, string email,
+            string passwordQuestion, string passwordAnswer,
             bool isApproved, object providerUserKey, out MembershipCreateStatus status)
         {
             throw new System.NotImplementedException();
         }
 
-        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion,
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password,
+            string newPasswordQuestion,
             string newPasswordAnswer)
         {
             throw new System.NotImplementedException();
@@ -109,12 +127,14 @@ namespace CountDown.Models.Security
             throw new System.NotImplementedException();
         }
 
-        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize,
+            out int totalRecords)
         {
             throw new System.NotImplementedException();
         }
 
-        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize,
+            out int totalRecords)
         {
             throw new System.NotImplementedException();
         }
